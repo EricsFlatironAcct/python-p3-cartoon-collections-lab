ac0f6aa6e17a5e8ac63efba3d4c4524f59470bdc
Eric Krukowski <erickruk@amazon.com>
2023-07-04 03:43:32 -0400

    Lab complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    [print(f"{(index+1)}. {dwarf}")for index, dwarf in enumerate(dwarves)]
+    
+
+def summon_captain_planet(calls):
+    return [f"{call.capitalize()}!" for call in calls]
     pass
 
-def summon_captain_planet():
-    pass
+def long_planeteer_calls(calls):
+    return len([call for call in calls if len(call)>4 ]) > 0
 
-def long_planeteer_calls():
-    pass
-
-def find_the_cheese():
-    pass
+def find_the_cheese(check_list):
+    cheese_list = ["cheddar", "gouda", "thyme"]
+    for check in check_list:
+        if check in cheese_list:
+            return check
+    return None
